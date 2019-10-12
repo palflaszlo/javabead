4c67e2a6c72d24d56d8a1ce30f9ca902eb0301af
plaszlo <55249413+palflaszlo@users.noreply.github.com>
2019-10-12 23:08:13 +0200

    Add files via upload

---
+++ toto_eredmeny.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package palfalvilaszlo_bead;
+
+/**
+ *
+ * @author Rumpelstiltskin
+ */
+public enum toto_eredmeny {
+    _1,
+    _2,
+    x
+}
